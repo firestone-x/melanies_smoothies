5ca9b5a380b9251549fe51b41c9f3ac765d8a219
firestone-x <75625984+firestone-x@users.noreply.github.com>
2024-03-11 00:10:51 +0100

    Update streamlit_app.py
    rearranging dataframe lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    st.write(ingredients_string)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        
 
     my_insert_stmt = f'''insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('{ingredients_string}', '{name_on_order}')'''
@@ -39,6 +41,4 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered, {name_on_order}! ', icon="✅")
         
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
